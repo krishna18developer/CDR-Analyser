cc3019804b12904303b150b7e495931bc6090678
Krishna Teja <krishnateja1812@gmail.com>
2023-05-29 12:20:24 +0530

    Add project files.

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CDR_Analyser
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dashboardMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // dashboardMenuStrip
+            // 
+            this.dashboardMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.dashboardMenuStrip.Name = "dashboardMenuStrip";
+            this.dashboardMenuStrip.Size = new System.Drawing.Size(800, 24);
+            this.dashboardMenuStrip.TabIndex = 0;
+            this.dashboardMenuStrip.Text = "menuStrip1";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dashboardMenuStrip);
+            this.MainMenuStrip = this.dashboardMenuStrip;
+            this.Name = "Dashboard";
+            this.Text = "CDR Analyser - Dashboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip dashboardMenuStrip;
+    }
+}
+
